16831e6fc96c40c72d55549127362d2c42f73e4b
Vladimir Shor <volli@webmedia.ee>
2010-03-22 11:28:26 +0200

    Resolver rewritten.

--- src/main/java/org/eclipse/mat/snapshot/registry/ClassSpecificNameResolverRegistry.java
+++ src/main/java/org/eclipse/mat/snapshot/registry/ClassSpecificNameResolverRegistry.java
@@ -11,15 +11,11 @@
 package org.eclipse.mat.snapshot.registry;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.mat.SnapshotException;
-import org.eclipse.mat.internal.MATPlugin;
 import org.eclipse.mat.internal.Messages;
 import org.eclipse.mat.snapshot.extension.IClassSpecificNameResolver;
 import org.eclipse.mat.snapshot.extension.Subject;
@@ -27,7 +23,6 @@ import org.eclipse.mat.snapshot.extension.Subjects;
 import org.eclipse.mat.snapshot.model.IClass;
 import org.eclipse.mat.snapshot.model.IObject;
 import org.eclipse.mat.util.MessageUtil;
-import org.eclipse.mat.util.RegistryReader;
 
 /**
  * Registry for name resolvers which resolve the names for objects of specific
@@ -42,59 +37,13 @@ public final class ClassSpecificNameResolverRegistry
     // //////////////////////////////////////////////////////////////
 
     /** inner class because RegitryReader API not public */
-    private static class RegistryImpl extends RegistryReader<IClassSpecificNameResolver>
+    private static class RegistryImpl 
     {
         private Map<String, IClassSpecificNameResolver> resolvers;
 
         public RegistryImpl()
         {
             resolvers = new HashMap<String, IClassSpecificNameResolver>();
-            init(MATPlugin.getDefault().getExtensionTracker(), MATPlugin.PLUGIN_ID + ".nameResolver"); //$NON-NLS-1$
-        }
-
-        @Override
-        public IClassSpecificNameResolver createDelegate(IConfigurationElement configElement)
-        {
-            try
-            {
-                IClassSpecificNameResolver resolver = (IClassSpecificNameResolver) configElement
-                                .createExecutableExtension("impl"); //$NON-NLS-1$
-
-                String[] subjects = extractSubjects(resolver);
-                if (subjects != null && subjects.length > 0)
-                {
-                    for (int ii = 0; ii < subjects.length; ii++)
-                        resolvers.put(subjects[ii], resolver);
-                }
-                else
-                {
-                    String msg = MessageUtil.format(Messages.ClassSpecificNameResolverRegistry_ErrorMsg_MissingSubject,
-                                    resolver.getClass().getName());
-                    Logger.getLogger(getClass().getName()).log(Level.WARNING, msg);
-                }
-
-                return resolver;
-            }
-            catch (CoreException e)
-            {
-                String msg = MessageUtil.format(
-                                Messages.ClassSpecificNameResolverRegistry_ErrorMsg_WhileCreatingResolver,
-                                configElement.getAttribute("impl")); //$NON-NLS-1$
-                Logger.getLogger(getClass().getName()).log(Level.SEVERE, msg, e);
-                return null;
-
-            }
-        }
-
-        @Override
-        protected void removeDelegate(IClassSpecificNameResolver delegate)
-        {
-            for (Iterator<IClassSpecificNameResolver> iter = resolvers.values().iterator(); iter.hasNext();)
-            {
-                IClassSpecificNameResolver r = iter.next();
-                if (r == delegate)
-                    iter.remove();
-            }
         }
 
         private String doResolve(IObject object)
@@ -128,6 +77,15 @@ public final class ClassSpecificNameResolverRegistry
             }
         }
 
+        public void registerResolver(IClassSpecificNameResolver resolver) {
+        	String[] subjects = extractSubjects(resolver);
+            if (subjects != null && subjects.length > 0)
+            {
+                for (int ii = 0; ii < subjects.length; ii++)
+                	instance().registry.resolvers.put(subjects[ii], resolver);
+            }
+        }
+
         private String[] extractSubjects(IClassSpecificNameResolver instance)
         {
             Subjects subjects = instance.getClass().getAnnotation(Subjects.class);
@@ -141,6 +99,29 @@ public final class ClassSpecificNameResolverRegistry
     }
 
     private static ClassSpecificNameResolverRegistry instance = new ClassSpecificNameResolverRegistry();
+    
+    static {
+    	instance.registry.registerResolver(new org.eclipse.mat.inspections.CommonNameResolver.StringResolver());
+    	instance.registry.registerResolver(new org.eclipse.mat.inspections.CommonNameResolver.StringBufferResolver());
+    	instance.registry.registerResolver(new org.eclipse.mat.inspections.CommonNameResolver.ThreadResolver());
+    	instance.registry.registerResolver(new org.eclipse.mat.inspections.CommonNameResolver.ThreadGroupResolver());
+    	instance.registry.registerResolver(new org.eclipse.mat.inspections.CommonNameResolver.ValueResolver());
+    	instance.registry.registerResolver(new org.eclipse.mat.inspections.CommonNameResolver.CharArrayResolver());
+    	instance.registry.registerResolver(new org.eclipse.mat.inspections.CommonNameResolver.ByteArrayResolver());
+    	instance.registry.registerResolver(new org.eclipse.mat.inspections.CommonNameResolver.URLResolver());
+    	instance.registry.registerResolver(new org.eclipse.mat.inspections.CommonNameResolver.AccessibleObjectResolver());
+    	instance.registry.registerResolver(new org.eclipse.mat.inspections.CommonNameResolver.FieldResolver());
+    	instance.registry.registerResolver(new org.eclipse.mat.inspections.CommonNameResolver.MethodResolver());
+    	instance.registry.registerResolver(new org.eclipse.mat.inspections.CommonNameResolver.ConstructorResolver());
+//    	instance.registry.registerResolver(new org.eclipse.mat.inspections.eclipse.EclipseNameResolver.EclipseClassLoaderResolver());
+//    	instance.registry.registerResolver(new org.eclipse.mat.inspections.eclipse.EclipseNameResolver.EclipseDefaultClassLoaderResolver());
+//    	instance.registry.registerResolver(new org.eclipse.mat.inspections.eclipse.EclipseNameResolver.StartupClassLoaderResolver());
+//    	instance.registry.registerResolver(new org.eclipse.mat.inspections.eclipse.EclipseNameResolver.RGBResolver());
+//    	instance.registry.registerResolver(new org.eclipse.mat.inspections.eclipse.EclipseNameResolver.PointResolver());
+//    	instance.registry.registerResolver(new org.eclipse.mat.inspections.eclipse.EclipseNameResolver.RectangleResolver());
+//    	instance.registry.registerResolver(new org.eclipse.mat.inspections.jetty.JettyNameResolvers.WebAppClassLoaderResolver());
+//    	instance.registry.registerResolver(new org.eclipse.mat.inspections.jetty.JettyNameResolvers.JasperLoaderResolver());
+    }
 
     public static ClassSpecificNameResolverRegistry instance()
     {
@@ -156,23 +137,6 @@ public final class ClassSpecificNameResolverRegistry
     private ClassSpecificNameResolverRegistry()
     {
         registry = new RegistryImpl();
-    }
-
-    /**
-     * Register class specific name resolver.
-     * 
-     * @param className
-     *            class name for which the class specific name resolver should
-     *            be used
-     * @param resolver
-     *            class specific name resolver
-     * @deprecated Use default extension mechanism: just implement interface and
-     *             register location via UI
-     */
-    @Deprecated
-    public static void registerResolver(String className, IClassSpecificNameResolver resolver)
-    {
-        instance().registry.resolvers.put(className, resolver);
     }
 
     /**
