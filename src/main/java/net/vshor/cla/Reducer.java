727bf5539778745df5a17d72afc5a74f14cdd023
jevgeni <jevgeni@zeroturnaround.com>
2010-10-24 17:16:06 +0300

    Experimenting with direct/indirect references

--- src/main/java/net/vshor/cla/Reducer.java
+++ src/main/java/net/vshor/cla/Reducer.java
@@ -1,10 +1,15 @@
 package net.vshor.cla;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 
 import org.eclipse.mat.collect.HashMapIntObject;
 import org.eclipse.mat.collect.IteratorInt;
+import org.eclipse.mat.collect.SetInt;
 import org.eclipse.mat.parser.internal.SnapshotFactory;
 import org.eclipse.mat.snapshot.ISnapshot;
 import org.eclipse.mat.util.ConsoleProgressListener;
@@ -14,7 +19,8 @@ public class Reducer {
   private static class ClassLoaderInfo {
     String name;
     int size = 0;
-    int incoming = 0;
+    int incomingIndirect = 0;
+    int incomingDirect = 0;
 
     public ClassLoaderInfo(String name) {        
       this.name = name;
@@ -22,7 +28,7 @@ public class Reducer {
 
     @Override
     public String toString() {     
-      return name + ", size = " + size + ", incoming = " + incoming;
+      return name + ", size = " + size + ", incoming direct = " + incomingDirect + ", incoming indirect = " + incomingIndirect;
     }
   }
 
@@ -42,14 +48,19 @@ public class Reducer {
     SnapshotFactory sf = new SnapshotFactory();
     ISnapshot snapshot = sf.openSnapshot(new File(args[0]),
         new HashMap<String, String>(), listener);
-    int[] retainedSet = snapshot.getRetainedSet(snapshot.getGCRoots(),
+    int[] retainedSetArr = snapshot.getRetainedSet(snapshot.getGCRoots(),
         listener);
+    
+    SetInt retainedSet = new SetInt();
+    for (int i : retainedSetArr) {
+      retainedSet.add(i);
+    }
 
-    System.out.println(retainedSet.length);		
+    System.out.println(retainedSetArr.length);		
 
     HashMapIntObject<ClassLoaderInfo> classloaders = new HashMapIntObject<ClassLoaderInfo>(500);
 
-    for (int obj : retainedSet) {
+    for (int obj : retainedSetArr) {
       int clId = snapshot.getClassOf(obj).getClassLoaderId();
       ClassLoaderInfo cli = classloaders.get(clId);
       if (cli == null) {
@@ -62,6 +73,9 @@ public class Reducer {
       for (int inObj : inbound) {
         boolean counts = true;
         
+        if (!retainedSet.contains(inObj))
+          continue;
+        
         do {
           if (snapshot.getClassOf(inObj).getClassLoaderId() == clId
               || inObj == clId) {
@@ -73,10 +87,12 @@ public class Reducer {
         } while (inObj != -1);
         
         if (counts)
-          cli.incoming++;
+          cli.incomingIndirect++;
       }
     }
 
+    List<ClassLoaderInfo> cliList = new ArrayList<Reducer.ClassLoaderInfo>();
+    
     for (IteratorInt i = classloaders.keys(); i.hasNext();) {
       int clId = i.next();
       ClassLoaderInfo cli = classloaders.get(clId);
@@ -85,21 +101,46 @@ public class Reducer {
       for (int inObj : inbound) {
         boolean counts = true;
         
+        if (!retainedSet.contains(inObj))
+          continue;
+        
+        int tmpInObj = inObj;
+        
         do {
-          if (snapshot.getClassOf(inObj).getClassLoaderId() == clId
-              || inObj == clId) {
+          if (snapshot.getClassOf(tmpInObj).getClassLoaderId() == clId
+              || tmpInObj == clId) {
             counts = false;
             break;
           }
           
-          inObj = snapshot.getImmediateDominatorId(inObj);
-        } while (inObj != -1);
+          tmpInObj = snapshot.getImmediateDominatorId(tmpInObj);
+        } while (tmpInObj != -1);
         
-        if (counts)
-          cli.incoming++;
-      }
+        if (counts) {
+          if (snapshot.getObject(clId).getTechnicalName().endsWith("0x1065ef1e0")) {
+            try {
+              System.out.println(snapshot.getObject(inObj).getTechnicalName());
+            } catch (Exception e) {
+              // TODO Auto-generated catch block
+              e.printStackTrace();
+            }
+          }
+            
+          cli.incomingDirect++;
+        }
+      }  
       
-      System.out.println(cli.toString());
+      cliList.add(cli);
+    }    
+    
+    Collections.sort(cliList, new Comparator<ClassLoaderInfo>() {
+      public int compare(ClassLoaderInfo o1, ClassLoaderInfo o2) {
+        return -Integer.valueOf(o1.size).compareTo(Integer.valueOf(o2.size));
+      }
+    });
+    
+    for (ClassLoaderInfo cli : cliList) {
+      System.out.println(cli);
     }
   }
 
